58a53cdb8708bb334c9263c62b76c4f0abf79fad
Vadim Sokoltsov <vforvad@gmail.com>
2026-03-17 17:45:16 +0100

    Add streaming homework

---
+++ de_zoomcamp/07-streaming/job_1h.py
@@ -0,0 +1,82 @@
+from pyflink.datastream import StreamExecutionEnvironment
+from pyflink.table import EnvironmentSettings, StreamTableEnvironment
+
+
+def create_tip_aggregated_sink(t_env):
+    table_name = 'processed_events_hourly_tips'
+    sink_ddl = f"""
+        CREATE TABLE {table_name} (
+            window_start TIMESTAMP(3),
+            window_end TIMESTAMP(3),
+            total_tip_amount DOUBLE,
+            PRIMARY KEY (window_start, window_end) NOT ENFORCED
+        ) WITH (
+            'connector' = 'jdbc',
+            'url' = 'jdbc:postgresql://postgres:5432/postgres',
+            'table-name' = '{table_name}',
+            'username' = 'postgres',
+            'password' = 'postgres',
+            'driver' = 'org.postgresql.Driver'
+        )
+    """
+    t_env.execute_sql(sink_ddl)
+    return table_name
+
+
+def create_events_source_kafka(t_env):
+    table_name = "events"
+    source_ddl = f"""
+        CREATE TABLE {table_name} (
+            PULocationID INT,
+            DOLocationID INT,
+            trip_distance DOUBLE,
+            total_amount DOUBLE,
+            passenger_count DOUBLE,
+            tip_amount DOUBLE,
+            lpep_pickup_datetime VARCHAR,
+            event_timestamp AS TO_TIMESTAMP(lpep_pickup_datetime, 'yyyy-MM-dd HH:mm:ss'),
+            WATERMARK FOR event_timestamp AS event_timestamp - INTERVAL '5' SECOND
+        ) WITH (
+            'connector' = 'kafka',
+            'properties.bootstrap.servers' = 'redpanda:29092',
+            'topic' = 'green-trips',
+            'scan.startup.mode' = 'earliest-offset',
+            'properties.auto.offset.reset' = 'earliest',
+            'format' = 'json',
+            'json.ignore-parse-errors' = 'true'
+        )
+    """
+    t_env.execute_sql(source_ddl)
+    return table_name
+
+
+def log_aggregation():
+    env = StreamExecutionEnvironment.get_execution_environment()
+    env.enable_checkpointing(10 * 1000)
+    env.set_parallelism(1)
+
+    settings = EnvironmentSettings.new_instance().in_streaming_mode().build()
+    t_env = StreamTableEnvironment.create(env, environment_settings=settings)
+
+    source_table = create_events_source_kafka(t_env)
+    aggregated_table = create_tip_aggregated_sink(t_env)
+
+    t_env.execute_sql(f"""
+        INSERT INTO {aggregated_table}
+        SELECT
+            window_start,
+            window_end,
+            SUM(tip_amount) AS total_tip_amount
+        FROM TABLE(
+            TUMBLE(
+                TABLE {source_table},
+                DESCRIPTOR(event_timestamp),
+                INTERVAL '1' HOUR
+            )
+        )
+        GROUP BY window_start, window_end
+    """).wait()
+
+
+if __name__ == '__main__':
+    log_aggregation()
